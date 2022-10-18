62db665edb77733470bec89ee2bcf92d3713c9df
haroldmud <haroldmudosa@gmail.com>
2022-10-18 18:03:05 +0200

    Made changes according to the CR

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "15s": "15px",
+        "8s": "8px",
+        "5s": "5px",
+      },
+      colors: {
+        grayf2: "#f2f2f2",
+      },
+      padding: {
+        "9s": "9px",
+        "5s": "5px",
+      },
+      width: {
+        "32r": "32rem",
+        "15r": "15rem",
+      },
+    },
   },
   plugins: [],
-}
+};
